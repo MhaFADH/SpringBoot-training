8e14b322df2432e3351a3bc4a044fc4ab990420d
MhaFADH <fadhlaoui.mohameda@gmail.com>
2022-10-30 15:21:14 +0100

    springb

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import com.example.demo.Students.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	@GetMapping
+	public List<Student> hello(){
+		return List.of(new Student(1L,"Mohamed",15, LocalDate.of(2002, Month.SEPTEMBER,18),"moha@gmail.com"));
 	}
 
 }
